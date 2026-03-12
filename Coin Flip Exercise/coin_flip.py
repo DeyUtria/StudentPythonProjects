78e7357f6a83659456ff70a712e8fa4b53020aff
DeyUtria <manuelparra3412@gmail.com>
2026-03-12 12:46:42 -0400

    Completed coin_flip.py file

---
+++ Coin Flip Exercise/coin_flip.py
@@ -0,0 +1,20 @@
+from random import randint
+
+results = []
+count = 0
+user_option = input("Please choose your option between heads or tails: ")
+
+while count < 1:
+    value = randint(0, 1)
+    if user_option == "Heads" and value == 0:
+        results.append(value)
+        count += 1
+        print("The result was " + str(value) + " so you won!")
+    elif user_option == "Tails" and value == 1:
+        results.append(value)
+        count += 1
+        print("The result was " + str(value) + " so you won!")
+    else:
+        results.append(value)
+        count += 1
+        print("The result was " + str(value) + " so you lost :(")
